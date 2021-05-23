60e5758eab7ce0f8e3abe601be5150dae93c7c23
Abhinav Yadav <abhinavy14@gmail.com>
2021-05-23 21:39:56 +0530

    changed one of the example, formated the code

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,40 @@
+use clap::{AppSettings, Clap};
 use std::fs::File;
 use std::io::Write;
-use clap::{AppSettings, Clap};
 
 #[derive(Clap)]
-#[clap(version = "0.1", author = "Abhinav <abhinavy14@gmail.com>", about = "prettify csv to table")]
+#[clap(
+    version = "0.1",
+    author = "Abhinav <abhinavy14@gmail.com>",
+    about = "prettify csv to table"
+)]
 #[clap(setting = AppSettings::ColoredHelp)]
-struct Opts{
-    #[clap( value_name = "FILE")]
+struct Opts {
+    #[clap(value_name = "FILE")]
     input: String,
     #[clap(short, long, default_value = "\n", about = "csv delimiter")]
     delimiter: char,
     #[clap(short, long, default_value = "table.txt", about = "output file_name")]
-    output: String
+    output: String,
 }
 
 fn main() {
     let opts: Opts = Opts::parse();
-    let Opts {input, delimiter, output} = opts;
+    let Opts {
+        input,
+        delimiter,
+        output,
+    } = opts;
     let data = std::fs::read_to_string(input).unwrap();
     let data = parse_csv(data, delimiter);
     println!("processing...");
     std::io::stdout().flush().unwrap();
     let table = process_string_to_table(&data, calc_max_len(&data));
     let mut f = File::create(&output).unwrap();
-    f.write_all(table.as_bytes()).expect("unable to write to file");
+    f.write_all(table.as_bytes())
+        .expect("unable to write to file");
     println!("\n{}\n\ntable saved to {}", &table, &output);
-    
+
     println!("DONE!!!!!!!!!");
 }
 
@@ -58,7 +67,15 @@ fn process_non_data_line(col_len_vec: &[usize]) -> String {
 fn process_data_line(row: usize, data: &[Vec<String>], col_len_vec: &[usize]) -> String {
     let mut ret_string = String::new();
     for (i, size) in col_len_vec.iter().enumerate() {
-        ret_string = format!("{2}{5}|{3}{:^1$}{4}", data[row][i], size + PADDING, ret_string, GREEN, RESET, PURPLE);
+        ret_string = format!(
+            "{2}{5}|{3}{:^1$}{4}",
+            data[row][i],
+            size + PADDING,
+            ret_string,
+            GREEN,
+            RESET,
+            PURPLE
+        );
     }
     format!("{}{1}|{2}\n", ret_string, PURPLE, RESET)
 }
